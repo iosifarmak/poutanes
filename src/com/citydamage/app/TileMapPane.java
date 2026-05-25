1fb5012c55bf06643d314cfb30ab6a06fe938e00
Aris G <arigj@example.com>
2026-05-25 18:14:01 +0300

    feat(map): TILE_SIZE, centerLat/Lon (Patras), zoom=13 defaults

--- src/com/citydamage/app/TileMapPane.java
+++ src/com/citydamage/app/TileMapPane.java
@@ -4,6 +4,12 @@ import javafx.scene.layout.Pane;
 
 public class TileMapPane extends Pane {
 
+    private static final int TILE_SIZE = 256;
+
+    private double centerLat = 38.2466;
+    private double centerLon = 21.7346;
+    private int    zoom      = 13;
+
     public TileMapPane() {
         setStyle("-fx-background-color: #ddd8cf;");
     }
